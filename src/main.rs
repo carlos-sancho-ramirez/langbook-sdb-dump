809522f38c4dadc9e7f655910ea1d22e1c2fe7ec
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2022-11-15 21:46:47 +0100

    Dump command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("Arguments found:");
+    for arg in args {
+        println!("  {}", arg);
+    }
 }
