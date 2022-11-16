05b77b6c2e9f99fb893917788bbca00bbd1be1ae
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2022-11-16 22:22:16 +0100

    Use Option for input_file availability

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("Arguments found:");
     let mut next_is_input = false;
-    let mut input_file_name = String::new();
-    let mut input_file_name_set = false;
+    let mut input_file_name: Option<String> = None;
     let mut is_first = true;
-    for arg in &args {
+    for arg in env::args() {
         if is_first {
             is_first = false;
         }
         else if next_is_input {
             next_is_input = false;
-            input_file_name_set = true;
-            input_file_name.push_str(arg);
+            input_file_name = Some(arg);
         }
         else if arg == "-i" {
-            if input_file_name_set {
-                panic!("Input file already set");
+            if input_file_name.is_none() {
+                next_is_input = true
             }
             else {
-                next_is_input = true
+                panic!("Input file already set");
             }
         }
         else {
@@ -29,10 +25,8 @@ fn main() {
         }
     }
 
-    if input_file_name_set {
-        println!("Reading file {}", input_file_name);
-    }
-    else {
-        panic!("Missing input file: try {} -i <sdb-file>", &args[0]);
+    match input_file_name {
+        Some(name) => println!("Reading file {}", name),
+        None => panic!("Missing input file: try {} -i <sdb-file>", env::args().next().expect("wtf?"))
     }
 }
